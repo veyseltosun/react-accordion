9f050778e675a74f7eb142e07109f0974b09f413
veyseltosun <postalarim1923@gmail.com>
2022-09-21 23:08:32 +0300

    final commit

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -6,7 +6,22 @@ import Soru from "./Question";
 function App() {
   const [questions, setQuestions] = useState(data);
 
-  return 
+  return (
+    <main>
+
+      <div className="container">
+        <h4>questions and answers about login</h4>
+        <section className="info">
+
+          {questions.map((question) =>{
+            return <Soru key={question.id} {...question}/>
+          })}
+
+        </section>
+
+      </div>   
+    </main>
+  )
 }
 
 export default App;
--- accordion/src/Question.jsx
+++ accordion/src/Question.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import data from "./data";
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import {useState} from "react";
 
-function Question() {
+
+function Question({title, info}) {
+    const [showInfo, setShowInfo] = useState(false);
+    const handleClick = () =>{
+        setShowInfo(!showInfo);
+    }
   return (
-    <div>Question</div>
+    <article className='question'>
+        <header>
+            <h4>{title}</h4>
+            <button className='btn' onClick={handleClick}>{showInfo ? <AiOutlineMinus/> : <AiOutlinePlus/>}</button>
+
+        </header>
+        {showInfo ? <p>{info}</p>: null }
+    </article>
   )
 }
 

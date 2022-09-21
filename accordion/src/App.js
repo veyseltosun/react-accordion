b82b093fd4fc1767c2b0aec014364d60770bf27c
veyseltosun <postalarim1923@gmail.com>
2022-09-21 22:26:36 +0300

    app

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,6 +1,11 @@
-import './App.css';
+import data from "./data";
+import { useState } from "react";
+import Soru from "./Question";
+
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return 
 }
 
